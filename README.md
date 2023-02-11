fe9d7bd21c1e9f9ea76cd1422a45887b54894160
qanx <64934214+qanx@users.noreply.github.com>
2023-02-11 21:01:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-BBm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-m6tcdv)
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="continer">
+        <div className="topBar">
+          <div className="left">
+            <span className="logo">sa ppl</span>
+          </div>
+          <div className="left"></div>
+        </div>
+      </div>
+      <div className="cir c1"></div>
+      <div className="cir c2"></div>
+      <div className="cir c3"></div>
+      <div className="cir c4"></div>
+      <div className="cir c5"></div>
+    </div>
+  );
+}
